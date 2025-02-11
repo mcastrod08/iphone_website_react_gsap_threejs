59faf3bf8379b64d6343d9d4ae013770efc308fc
Maria Castro <mcastrod08@gmail.com>
2025-02-11 17:27:21 +0100

    hero section

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,13 @@
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
 import bag from "/assets/images/bag.svg";
-
-
+import smallmv from "/assets/videos/smallHero.mp4";
+import hmv from "/assets/videos/hero.mp4";
 
 
 
 export const appleImg = apple;
 export const searchImg = search;
 export const bagImg = bag;
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
