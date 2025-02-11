dd0ae988a98cefffc4880fe9a174aecee43226db
Maria Castro <mcastrod08@gmail.com>
2025-02-11 12:17:56 +0100

    nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const Hero = () => {
+
+  useGSAP(() => {
+    gsap.to('#hero', {opacity: 1, delay: 2})
+  }, [])
+
   return (
-    <div>Hero</div>
+    <section className="w-full nav-height relative">
+      <div className="h-5/6 flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="header flex justify-between ">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="apple-logo" width="25px" height="25px"/>
-        <div>List</div>
-        <img src={bagImg} alt="bag-image" width="25px" height="25px"/>
-        <img src={searchImg} alt="search-image" width="25px" height="25px"/>
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div key={nav} className="nav-items text-sm text-gray-300 hover:text-amber-300 transition-all cursor-pointer">
+              {nav}
+            </div>
+          ))}
+        </div>
+        <div className='flex gap-6 items-baseline max-sm:justify-end max-sm:flex-1'>
+          <img src={bagImg} alt="bag-image" width="25px" height="25px"/>
+          <img src={searchImg} alt="search-image" width="25px" height="25px"/>
+        </div>
       </nav>
     </header>
   )
