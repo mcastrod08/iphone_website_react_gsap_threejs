554fc20d4bbc33ec3129deb3c1156730a31c114c
Maria Castro <mcastrod08@gmail.com>
2025-02-17 13:03:57 +0100

    models section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ModelView from './ModelView'
 import { yellowImg } from '../utils'
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { View } from '@react-three/drei'
-import { models } from '../constants'
+import { models, sizes } from '../constants'
+import { animateWithGsapTimeline } from '../utils/animations'
 
 const Model = () => {
 
@@ -34,6 +35,23 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
 
   return (
     <section className="common-padding">
@@ -85,7 +103,7 @@ const Model = () => {
               {model.title}
             </p>
 
-            <div className="flex-center " style={{margin: '20px 0'}}>
+            <div className="flex-center gap-3" style={{margin: '20px 0'}}>
               <ul className='color-container'>
                 {models.map((item, i) => (
                   <li 
@@ -99,6 +117,20 @@ const Model = () => {
                 ))}
               </ul>
               
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span 
+                    key={label} 
+                    className="size-btn cursor-pointer"
+                    style={{ backgroundColor: size === value ? 'white' : 'transparent', 
+                      color: size === value ? 'black' : 'white'
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
 
           </div>
